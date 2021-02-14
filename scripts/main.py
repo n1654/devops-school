ba58887920356afbb23d568d27c9d8ff34eee0fd
overdoze14 <42451885+overdoze14@users.noreply.github.com>
2021-02-15 02:49:37 +0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -7,6 +7,6 @@ timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with open('artifact.txt', 'w') as f:
     f.write(f'# {timestamp}')
-    f.write(f'os.environ['INPUT_KEY']')
+    f.write(f'{os.environ['INPUT_KEY']}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
