d8779e45c23eb17c3a565f615ce726599e948882
overdoze14 <42451885+overdoze14@users.noreply.github.com>
2021-02-15 02:41:59 +0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,12 @@
+import os
 from datetime import datetime
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-with open('log.md', 'w') as f:
+with open('artifact.txt', 'w') as f:
     f.write(f'# {timestamp}')
+    f.write(f'os.environ['INPUT_USERINPUT']')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
