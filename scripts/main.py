7d1b3efe70108fdb2f67117e652df9478afb3344
overdoze14 <42451885+overdoze14@users.noreply.github.com>
2021-02-15 03:16:48 +0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-keu = os.environ['INPUT_KEY']
+key = os.environ['INPUT_KEY']
 with open('artifact.txt', 'w') as f:
     f.write(f'# {timestamp}   {key}   ')
     
